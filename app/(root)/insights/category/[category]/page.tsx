f3152eabcff6328283e8e2d2695d07990ac2c85c
Neon-jeff <omaksteejay@gmail.com>
2025-02-19 18:32:47 +0100

    feat:post seo and meta data fix

--- app/(root)/insights/category/[category]/page.tsx
+++ app/(root)/insights/category/[category]/page.tsx
@@ -18,9 +18,9 @@ const fetchPost = async (category: string): Promise<Array<PostType>> => {
   return data;
 };
 
-const Insights = async ({ params }) => {
+const Insights = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { category } = await params;
-  console.log(category)
+  console.log(category);
   const posts = await fetchPost(category);
   return (
     <div className="pb-36 bg-white  place-items-center  mx-auto ">
@@ -32,16 +32,16 @@ const Insights = async ({ params }) => {
           {/* <LatestNews/> */}
           {/* all news layout here */}
 
-
-
           <div className="lg:px-20 lg:mt-10">
             <p className="border mb-6 bg-gray-50 w-fit p-2 max-md:px-5 text-sm rounded-full">
               All Posts
             </p>
 
-            {
-            posts.length == 0 && <div className='p-5 bg-green-50 rounded-xl text-green-950 '><p>No posts found for this category</p></div>
-          }
+            {posts.length == 0 && (
+              <div className="p-5 bg-green-50 rounded-xl text-green-950 ">
+                <p>No posts found for this category</p>
+              </div>
+            )}
             <div className="grid grid-cols-3 gap-y-16 gap-10 max-md:gap-y-20 lg:mt-10 mt-5 max-md:grid-cols-2 max-sm:grid-cols-1">
               {posts.map((item, index) => (
                 <NewsCard
