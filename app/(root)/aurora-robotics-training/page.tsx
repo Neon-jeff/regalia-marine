10562adf38ad5486cb73dce04ef856026157dbe4
Neon-jeff <omaksteejay@gmail.com>
2025-08-15 23:52:23 +0100

    feat: curriculm link change

--- app/(root)/aurora-robotics-training/page.tsx
+++ app/(root)/aurora-robotics-training/page.tsx
@@ -17,7 +17,7 @@ const AuroraTrainingPage = () => {
       <AuroraNav />
       <AuroraHero/>
       <div className="flex max-md:flex-col max-md:w-full w-fit mx-auto gap-5">
-        <Link target="_blank" href={'https://bakel-bakel.github.io/robotics-core-workshop-oct2025/'} className="bg-aurora-main px-6 py-4 flex items-center justify-center gap-2 rounded-full text-black">View Curriculum <IconArrowUpRight/></Link>
+        <Link target="_blank" href={'https://robotics-weld.vercel.app/'} className="bg-aurora-main px-6 py-4 flex items-center justify-center gap-2 rounded-full text-black">View Curriculum <IconArrowUpRight/></Link>
          <Link target="_blank" href = {'https://www.upwork.com/freelancers/bakel'} className="bg-transparent border px-6 py-4 border-white flex items-center justify-center gap-2 rounded-full">Meet the tutor <IconArrowRight/></Link>
       </div>
       <div className="mx-auto text-center text-gray-200 bg-aurora-main/10 border border-aurora-main/20 w-fit p-5 rounded-xl space-y-2">
